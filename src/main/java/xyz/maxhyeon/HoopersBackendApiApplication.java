15b85a06f5016c9bf38dc1c6f80c45efb653e5ff
maxHyeon <maxhyeon.park@gmail.com>
2024-07-14 12:39:55 +0900

    init

---
+++ src/main/java/xyz/maxhyeon/HoopersBackendApiApplication.java
@@ -0,0 +1,13 @@
+package xyz.maxhyeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoopersBackendApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoopersBackendApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/maxhyeon/HoopersBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.maxhyeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoopersBackendApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
